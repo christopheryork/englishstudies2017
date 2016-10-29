908f27b10cebfe553bdf5e8e3d895f197cf55281
Christopher W York <christopher.w.york@gmail.com>
2016-10-29 02:13:26 +0100

    Boxplot of author performances

---
+++ author_performances.js
@@ -0,0 +1,98 @@
+import * as d3 from 'd3'
+
+const width = 50
+const height = 1024
+const margins = { left: 150, top: 20, right: 150, bottom: 20 }
+
+const radius = 2.5
+
+let shorten = function(x, len) {
+  if(!x) return ""
+  x = "" + x
+  if(!len) return x
+
+  let hard = x.search(/\s*[(,;]/)
+  hard = (hard > -1) ? hard : Infinity
+  let soft = x.indexOf(' ', len)
+  soft = (soft > -1) ? soft : Infinity
+
+  return x.slice(0, Math.min(hard, soft))
+}
+
+let fmt = d3.format(',d')
+
+
+d3.text('author_performances.css', (err, styles) => {
+  if(err) throw err
+
+  d3.csv('author_performances.csv', (err, data) => {
+    if(err) throw err
+
+    data.forEach( (d) => d.performances = +d.performances )
+    data.sort( (a,b) => d3.ascending(a.performances, b.performances) )
+
+    let breaks = [ .03, .25, .5, .75, .97 ]
+    let thresholds = breaks.map( (q) => {
+      return d3.quantile(data, q, (d) => d.performances )
+    })
+
+    let svg = d3.select('body').append('svg')
+      .attr('width', width + margins.left + margins.right)
+      .attr('height', height + margins.top + margins.bottom)
+
+    svg.append('style')
+        .text(styles)
+
+    svg = svg.append('g')
+        .attr('transform', 'translate(' + [margins.left, margins.top] + ')')
+
+    let y = d3.scalePow()
+      .exponent(0.5)
+      .domain(d3.extent(data, (d) => d.performances))
+      .range([height, 0])
+
+    let threshold = svg.selectAll('.threshold')
+      .data(thresholds)
+      .enter().append('g')
+        .attr('class', 'threshold')
+        .attr('transform', (d) => 'translate(0,' + Math.round(y(d)) + ')')
+
+    threshold.append('path')
+      .attr('d', (d) => 'M0 0H' + width)
+    threshold.append('text')
+      .attr('x', width)
+      .attr('dx', 10)
+      .attr('dy', '0.3em')
+      .text( (d,i) => fmt(d) + (i === 2 ? ' performance evenings' : '') )
+
+    svg.selectAll('.side')
+      .data(d3.pairs(thresholds))
+      .enter().append('path')
+        .attr('class', 'side')
+        .attr('d', (d,i) => {
+          let center = i === 0 || i === breaks.length - 2
+          return center ? 'M' + (width / 2) + ' ' + Math.round(y(d[0])) + 'V' + Math.round(y(d[1]))
+                        : 'M0 ' + Math.round(y(d[0])) + 'V' + Math.round(y(d[1])) +
+                          'M ' + width + ' ' + Math.round(y(d[0])) + 'V' + Math.round(y(d[1]))
+        })
+
+    let outdata = data.filter( (d) => d.performances < d3.min(thresholds) || d.performances > d3.max(thresholds) )
+
+    let outlier = svg.selectAll('.outlier')
+      .data(outdata)
+      .enter().append('g')
+        .attr('class', 'outlier')
+        .attr('transform', (d) => 'translate(' + [width/2, Math.round(y(d.performances))] + ')')
+
+    outlier.append('circle')
+      .attr('r', radius)
+      .attr('fill', 'none')
+
+    outlier.append('text')
+      .attr('dy', '0.3em')
+      .attr('x', (d,i) => (i % 2 ? 1 : -1) * 10)
+      .attr('text-anchor', (d,i) => i % 2 ? 'start' : 'end')
+      .text( (d) => [shorten(d.author, 10), fmt(d.performances)].join('  '))
+  })
+
+})
